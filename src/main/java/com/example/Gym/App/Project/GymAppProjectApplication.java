9e47670153b2c06c5683a973b6df90c0ce9e9c7c
Dawid Blachowicz <david.blachowicz@gmail.com>
2022-07-06 14:20:16 +0200

    Project and repo init

---
+++ src/main/java/com/example/Gym/App/Project/GymAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gym.App.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gym/App/Project/GymAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gym.App.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
